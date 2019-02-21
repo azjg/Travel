b3a5ae12fe1779ee2892cf40e8bce4891f5f41b7
zhangjigang <517855672@qq.com>
2019-02-22 05:38:53 +0800

    city

---
+++ src/pages/city/components/Alphabet.vue
@@ -0,0 +1,32 @@
+<template>
+  <ul class="list">
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: 'CityAlphabet'
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~styles/varibles'
+  .list
+    position absolute
+    top 1.58rem
+    right 0
+    bottom 0
+    display flex
+    flex-direction column
+    justify-content center
+    width .4rem
+    .item
+      text-align center
+      line-height .44rem
+      color $bgColor
+</style>
