564501fbd4e1e8d0a3a950660d874bb5902fdd00
zhangjigang <517855672@qq.com>
2019-02-27 09:19:43 +0800

    add ajax

--- src/pages/detail/Detail.vue
+++ src/pages/detail/Detail.vue
@@ -1,19 +1,64 @@
 <template>
   <div>
-    <detail-banner></detail-banner>
+    <detail-banner :sightName="sightName" :bannerImg="bannerImg" :bannerImgs="gallaryImgs"></detail-banner>
+    <detail-header></detail-header>
+    <div class="content">
+      <detail-list :list="list"></detail-list>
+    </div>
   </div>
 </template>
 
 <script>
 import DetailBanner from './components/Banner'
+import DetailHeader from './components/Header'
+import DetailList from './components/List'
+import axios from 'axios'
 export default {
   name: 'Detail',
   components: {
-    DetailBanner
+    DetailBanner,
+    DetailHeader,
+    DetailList
+  },
+  data () {
+    return {
+      sightName: '',
+      bannerImg: '',
+      gallaryImgs: [],
+      list: []
+    }
+  },
+  methods: {
+    getDetailInfo () {
+      axios.get('/api/detail.json', {
+        params: {
+          id: this.$route.params.id
+        }
+      }).then(res => {
+        // console.log(res)
+        res = res.data
+        // console.log(res)
+        if (res.ret && res.data) {
+          const data = res.data
+          console.log(data)
+          this.sightName = data.sightName
+          this.bannerImg = data.bannerImg
+          this.gallaryImgs = data.gallaryImgs
+          this.list = data.categoryList
+        }
+      })
+    }
+  },
+  created () {
+    this.getDetailInfo()
+  },
+  activated () {
+    this.getDetailInfo()
   }
 }
 </script>
 
 <style lang="stylus" scoped>
-
+  .content
+    height 50rem
 </style>
